20203cace5395af8159c4ded1f7eb781821e0fde
Shivammali <malishivam34@gmail.com>
2023-06-18 02:30:33 +0530

    code

--- PortfolioShivam/src/app/home/home.component.ts
+++ PortfolioShivam/src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent  implements OnInit {
+ public wordList:string[]=['Interactive','Appealing','Amazing']
+  public currentWord:string=''
+ngOnInit(): void {
+  this.changingWord(); 
 }
+ 
+  changingWord(){
+    let index = 0;
+    setInterval(() => {
+      this.currentWord = this.wordList[index];
+      index = (index + 1) % this.wordList.length;
+      console.log(this.currentWord)
+    }, 2000);
+  }
+ 
+}
+
